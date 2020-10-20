aa2d4a1b765e369ac6f6eb815e32cedcd6920118
Michał Kadlof <m.kadlof@cent.uw.edu.pl>
2020-10-20 13:52:18 +0200

    cleanup

--- atomGenerator.py
+++ atomGenerator.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from random import uniform
-import sys
-import argparse
 
 
 def randomAtoms(n, d):
@@ -12,12 +10,13 @@ def randomAtoms(n, d):
         d - rozmiar pudełka
     """
     atoms = []
-    for i in xrange(n):
-        x = uniform(0,d)
-        y = uniform(0,d)
-        z = uniform(0,d)
-        atoms.append((x,y,z))
+    for _ in xrange(n):
+        x = uniform(0, d)
+        y = uniform(0, d)
+        z = uniform(0, d)
+        atoms.append((x, y, z))
     return atoms
+
 
 def polimerLine(n):
     """
@@ -28,9 +27,10 @@ def polimerLine(n):
     for i in xrange(n):
         x = i
         y = 0
-        z = 0 
-        atoms.append((x,y,z))
+        z = 0
+        atoms.append((x, y, z))
     return atoms
+
 
 def polimerCircle(n):
     """
@@ -41,14 +41,15 @@ def polimerCircle(n):
     from math import pi
     from math import radians
     atoms = []
-    angleIncrement = 360 / float(n)
-    radius = 1/(2*sin(radians(angleIncrement)/2.)) # Odległość co 1
+    angle_increment = 360 / float(n)
+    radius = 1 / (2 * sin(radians(angle_increment) / 2.))  # Odległość co 1
     for i in xrange(n):
-        x = radius * cos(angleIncrement * i * pi/180)    
-        y = radius * sin(angleIncrement * i * pi/180)    
+        x = radius * cos(angle_increment * i * pi / 180)
+        y = radius * sin(angle_increment * i * pi / 180)
         z = 0
-        atoms.append((x,y,z))
+        atoms.append((x, y, z))
     return atoms
-    
+
+
 if __name__ == '__main__':
     main()
--- saveMyStructure.py
+++ saveMyStructure.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pointConverter import *
-from atomGenerator import *
 import sys
+
+from atomGenerator import *
+from pointConverter import *
+
 
 def main():
     fname = sys.argv[1]
-    atoms = polimerCircle(40) 
-    atoms = polimerLine(40) 
-    atoms = randomAtoms(40, 10)
-    savePointsAsGro(atoms, "{}.gro".format(fname), "okręg 40 atomów")
+    # atoms = polimerCircle(40)
+    atoms = polimerLine(40)
+    # atoms = randomAtoms(40, 10)
     savePointsAsPdb(atoms, "{}.pdb".format(fname))
-    savePointsAsXYZ(atoms, "{}.xyz".format(fname))
+
 
 if __name__ == '__main__':
     main()
